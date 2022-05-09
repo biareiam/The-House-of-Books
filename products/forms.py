ba734e2a7f451953e4c35e79ac772eb143265459
biareiam <biareiam@gmail.com>
2022-05-09 18:37:13 +0000

    fixing the image display on the add/edit product forms

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-#from .widgets import CustomsCleareableFileInput
+from .widgets import CustomsCleareableFileInput
 from .models import Product, Category
 
 
@@ -10,7 +10,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    #image = forms.ImageField(label='Image', required=False, widget=CustomsCleareableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomsCleareableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
