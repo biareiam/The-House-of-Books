75b436d23adea72c5e5dd24fd72dbc6486b9aa90
biareiam <biareiam@gmail.com>
2022-05-23 18:53:13 +0000

    fixing the categories display on the product page

--- house_books/settings.py
+++ house_books/settings.py
@@ -132,6 +132,8 @@ else:
         }
     }
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
