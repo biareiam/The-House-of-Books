789cf39d3fa48403745bb259d47e6e441c0f7dfc
biareiam <biareiam@gmail.com>
2022-06-15 15:56:52 +0000

    creating the privacy policy file and styling it

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
+""" Library """
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
@@ -8,5 +9,17 @@ def index(request):
     paginate_by = 6
 
     return render(request, "home/index.html")
+
+
+def privacy(request):
+    """ A view to return the privacy page """
+
+    return render(request, 'home/privacy.html')
+
+
+def terms(request):
+    """ A view to return the terms and conditions page """
+
+    return render(request, 'home/terms.html')
 
 
