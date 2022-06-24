beb765743e7c41aa28c24eaa29949bae29d2bc19
biareiam <biareiam@gmail.com>
2022-06-24 17:33:51 +0000

    adding events to the admin portal

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,39 @@
+"""Set up of django admin site for events app"""
 from django.contrib import admin
+from .models import County, Event, Comment
 
-# Register your models here.
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    """
+    County model - admin site set up:
+    Show all fields in list display
+    """
+    list_display = ('friendly_name', 'name')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """
+    Event model - admin set up: fields to show in list view,
+    Allow filtering by county, and search by name.
+    """
+    list_display = (
+        'name', 'location', 'county', 'date', 'event_description', 'date_passed',
+        )
+    list_filter = ('county',)
+    search_fields = ['name', 'location', ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin set up for Comments model - show fields in list display and
+    allow filtering of comments by event or comment author, add link
+    fields and allow search on comment.
+    """
+    readonly_fields = ['id', ]
+    list_display = ('id', 'author_comment', 'event', 'created_on', 'comment', )
+    list_filter = ('event', 'author_comment', )
+    list_display_links = ('id', 'created_on', 'comment',)
+    search_fields = ['comment', ]
