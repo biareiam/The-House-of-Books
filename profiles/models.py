edada919192f78b6a2dba6cd367002cc48ce047a
biareiam <biareiam@gmail.com>
2022-06-25 18:45:41 +0000

    fixing the profile models in attempt to fix the 500 error on heroku

--- profiles/models.py
+++ profiles/models.py
@@ -1,56 +1,58 @@
-""" libraries """
+"""Models for profiles app """
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from events.models import Event
 
 from django_countries.fields import CountryField
+
+from events.models import Event
 
 
 class UserProfile(models.Model):
     """
-    A user profile model for maintaining default
-    delivery information and order history
+    User Profile model, extending User model with onetoone relationship.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20,
-                                            null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40,
-                                            null=True, blank=True)
-    default_county = models.CharField(max_length=80,
-                                      null=True, blank=True)
-    default_postcode = models.CharField(max_length=20,
-                                        null=True, blank=True)
-    default_country = CountryField(blank_label='Country',
-                                   null=True, blank=True)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True
+        )
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True
+        )
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Choose country from dropdown',
+        null=True,
+        blank=True)
 
     def __str__(self):
+        """string method, return user name"""
         return self.user.username
 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update the user profile
+    Create instance of UserProfile each time instance of User is created.
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
     instance.userprofile.save()
 
 
 class SavedEventList(models.Model):
     """
-    To hold the event(s) that a user can save to their profile.S
+    To hold the event(s) that a user can save to their profile.
     """
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    event = models.ManyToManyField(Event(), blank=True)
+    event = models.ManyToManyField(Event, blank=True)
 
     def __str__(self):
         """string method, return 'username's saved events'"""
-        return f'{self.user.username}\'s saved events'
+        return f'{self.user.user.username}\'s saved events'
