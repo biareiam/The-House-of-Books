74bd06e10230af0178a61d2c32e02cbbf8bcef47
biareiam <biareiam@gmail.com>
2022-06-26 11:39:24 +0000

    running the tests on the product page

--- house_books/settings.py
+++ house_books/settings.py
@@ -136,6 +136,15 @@ else:
         }
     }
 
+# uncomment to run the tests
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+    #}
+#}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
