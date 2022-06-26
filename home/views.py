301e50bcd5410b3080421601558f21d724996d8d
biareiam <biareiam@gmail.com>
2022-06-26 07:53:30 +0000

    adding the most sold books back to the home page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
+import pandas as pd
+from checkout.models import OrderLineItem
+from django.db.models import Count
 
 def index(request):
-    """ Return the index page """
+    """ A view to return the index page """
+    data = OrderLineItem.objects.all().annotate(dcount=Count('quantity')).order_by('-quantity')
+    products = []
+    for row in data:
+        products.append({
+            'id':row.product.id,
+            'category':row.product.category,
+            'sku':row.product.sku,
+            'name':row.product.name,
+            'author':row.product.author,
+            'description':row.product.description,
+            'price':row.product.price,
+            'rating':row.product.rating,
+            'image_url':row.product.image_url,
+            'image':row.product.image.url
+        })
 
-    paginate_by = 6
+    products_df = pd.DataFrame(products)
 
-    return render(request, "home/index.html")
+    bool_series = products_df.duplicated()
+    products_df_dup = products_df[~bool_series]
+  
+    context = {
+        'products':products_df_dup.head(4).to_dict('records')
+    }
+    
+
+    return render(request, 'home/index.html',context)
