48d63a28a3a5d857cd6edefa11918cecee87a25d
biareiam <biareiam@gmail.com>
2022-06-26 07:39:26 +0000

    fixing the size of the images on the home page

--- home/views.py
+++ home/views.py
@@ -1,47 +1,36 @@
+""" Library """
 from django.shortcuts import render
 import pandas as pd
-from checkout.models import OrderLineItem
 from django.db.models import Count
+from checkout.models import OrderLineItem
 
 
 def index(request):
     """ A view to return the index page """
-    data = OrderLineItem.objects.all().annotate(dcount=Count('quantity')).order_by('-quantity')
+    data = OrderLineItem.objects.all().annotate(dcount=Count(
+                                                'quantity')).order_by(
+                                                '-quantity')
     products = []
     for row in data:
         products.append({
-            'id':row.product.id,
-            'category':row.product.category,
-            'sku':row.product.sku,
-            'name':row.product.name,
-            'author':row.product.author,
-            'description':row.product.description,
-            'price':row.product.price,
-            'rating':row.product.rating,
-            'image_url':row.product.image_url,
-            'image':row.product.image.url
+            'id': row.product.id,
+            'category': row.product.category,
+            'sku': row.product.sku,
+            'name': row.product.name,
+            'author': row.product.author,
+            'description': row.product.description,
+            'price': row.product.price,
+            'rating': row.product.rating,
+            'image_url': row.product.image_url,
+            'image': row.product.image.url
         })
 
     products_df = pd.DataFrame(products)
 
     bool_series = products_df.duplicated()
     products_df_dup = products_df[~bool_series]
-  
     context = {
-        'products':products_df_dup.head(3).to_dict('records')
+        'products': products_df_dup.head(4).to_dict('records')
     }
-    
     return render(request, 'home/index.html', context)
-
-
-def privacy(request):
-    """ A view to return the privacy page """
-
-    return render(request, 'home/privacy.html')
-
-
-def terms(request):
-    """ A view to return the terms and conditions page """
-
-    return render(request, 'home/terms.html')
 
