be1dcf5bc81f9b592bb198fdbaca5672ab91e336
biareiam <biareiam@gmail.com>
2022-06-26 07:50:11 +0000

    trying to fix bug on heroku

--- home/views.py
+++ home/views.py
@@ -1,36 +1,10 @@
-""" Library """
 from django.shortcuts import render
-import pandas as pd
-from django.db.models import Count
-from checkout.models import OrderLineItem
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    """ A view to return the index page """
-    data = OrderLineItem.objects.all().annotate(dcount=Count(
-                                                'quantity')).order_by(
-                                                '-quantity')
-    products = []
-    for row in data:
-        products.append({
-            'id': row.product.id,
-            'category': row.product.category,
-            'sku': row.product.sku,
-            'name': row.product.name,
-            'author': row.product.author,
-            'description': row.product.description,
-            'price': row.product.price,
-            'rating': row.product.rating,
-            'image_url': row.product.image_url,
-            'image': row.product.image.url
-        })
+    """ Return the index page """
 
-    products_df = pd.DataFrame(products)
+    paginate_by = 6
 
-    bool_series = products_df.duplicated()
-    products_df_dup = products_df[~bool_series]
-    context = {
-        'products': products_df_dup.head(4).to_dict('records')
-    }
-    return render(request, 'home/index.html', context)
-
+    return render(request, "home/index.html")
